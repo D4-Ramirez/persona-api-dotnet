1fbad5543eb01d0cb2c82b160016baa9f9424672
David Ramirez <ramirezmonroyd@gmail.com>
2022-10-16 16:11:34 -0500

    First lab finished

--- personaapi-dotnet/Program.cs
+++ personaapi-dotnet/Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<personaapi_dotnet.Models.Entities.persona_dbContext>(options =>
+    options.UseSqlServer("Name=ConnectionStrings:MainConnection" ?? throw new InvalidOperationException("Connection string 'persona_dbContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
