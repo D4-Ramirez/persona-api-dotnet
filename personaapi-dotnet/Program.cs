ea19cf61b68f7cafb7f32f75c9450289bbbe613f
David Ramirez <ramirezmonroyd@gmail.com>
2022-10-16 04:20:36 -0500

    Recreated the dotnet project

--- personaapi-dotnet/Program.cs
+++ personaapi-dotnet/Program.cs
@@ -1,6 +1,3 @@
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using personaapi_dotnet.Models.Entities;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,11 +9,7 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
 }
-
-app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
